feb3897fd1a1a45ff3f5ea27e933fae20c8d6d40
CescoMoretti <71732702+CescoMoretti@users.noreply.github.com>
2022-06-16 10:05:30 +0200

    dataset testing (working)

--- utils/dataset_test/read_video.py
+++ utils/dataset_test/read_video.py
@@ -1,6 +1,6 @@
 import cv2 as cv
-
-capture = cv.VideoCapture('Videos/000.avi')
+#put the path in your pc
+capture = cv.VideoCapture('c:/Users/cesco/OneDrive/Desktop/computer vision/Project group 25/MOTSynth_1/000.mp4') 
 while(True):
     isTrue, frame = capture.read()
     if not isTrue:
