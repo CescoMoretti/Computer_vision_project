26b39fe88ddd2ed2571abb7d919b907a207bd6e3
CescoMoretti <71732702+CescoMoretti@users.noreply.github.com>
2023-05-23 00:06:38 +0200

    main: crete main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 from pipeline_object.pipeline import Pipeline
 import os
+import argparse
 
-this_path = os.getcwd()
+this_path = os.getcwd
 
 def main():
-    pipeline = Pipeline()
-    for i in range(8):
+    parser = argparse.ArgumentParser(description='Get frames from videos')
+    parser.add_argument(
+        '--video_dir', help='Directory hosting videos', required=True)
+    parser.add_argument('--out_dir', help='Directory of output', required=True)
+
+    args = parser.parse_args()
+
+    video_dir = args.video_dir
+    out_dir = args.out_dir
+    print(video_dir)
+    pipeline = Pipeline(out_dir, video_dir)
+    for i in range(500):
         pipeline.process_frame()
 
 if __name__ == "__main__":
