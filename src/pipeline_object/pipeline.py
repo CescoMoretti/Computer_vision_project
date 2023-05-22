f13676c8c86369b182b0c7540c22fb857e6b03e7
CescoMoretti <71732702+CescoMoretti@users.noreply.github.com>
2023-05-23 00:08:45 +0200

    pipeline: add detection and uoi to fuse bb

--- src/pipeline_object/pipeline.py
+++ src/pipeline_object/pipeline.py
@@ -1,24 +1,49 @@
-from people_detection.detector import People_detector
+from detector.detector import Detector
 from utils.functions.frame_extractor import Frame_extractor
 import os
+from shapely.geometry import Polygon
 
 
-
+this_path = os.getcwd()
 class Pipeline():
-    def __init__(self):
+    def __init__(self, out_dir, video_dir):
         self.this_path = os.getcwd()
-        self.detector = People_detector(self.this_path+'/src/people_detection/YOLOv3/yolov3.weights',
-                        self.this_path+'/src/people_detection/YOLOv3/yolov3.cfg',
-                        self.this_path+'/src/people_detection/YOLOv3/coco.names')
-        self.frame_extractor = Frame_extractor('c:/Users/cesco/OneDrive/Desktop/computer vision/Project group 25/test_out/')
-        self.frame_extractor.load_video("C:/Users/cesco/OneDrive/Desktop/computer vision/Project group 25/MOTSynth_1/000.mp4")
+        self.people_detector = Detector(this_path+'/src/detector/YOLOv4/yolov4_coco.weights',
+                        this_path+'/src/detector/YOLOv4/yolov4.cfg',
+                        this_path+'/src/detector/YOLOv4/person.names')
+        self.head_detector = Detector(this_path+'/src/detector/YOLOv4/yolov4-head_last.weights',
+                        this_path+'/src/detector/YOLOv4/yolov4-head.cfg',
+                        this_path+'/src/detector/YOLOv4/head.names')
+        self.frame_extractor = Frame_extractor()
+        self.frame_extractor.load_video(video_dir)
+
 
     def process_frame(self):
-        frame = self.frame_extractor.extract()
-        id_img, img_path= self.detector.detectPeople(frame)
+        frame, finished_video = self.frame_extractor.extract()
+        people_classIds, people_scores, people_boxes = self.people_detector.detect(frame)
         
-        #bouniding_box = detector.findpeople(img)
-        #histogram = histogram_finder.getHistogram(bouniding_box)
-        #distance = distance_finder.findDistance(bouniding_box) -->creare oggetto per estrarre teste
-        #histogram finder / salva dati riconoscimento
-        #distance findere
+        head_classIds, head_scores, head_boxes = self.head_detector.detect(frame)
+        for h_box in head_boxes:
+            for p_box in people_boxes:
+                iou = Pipeline.calculate_iou(h_box, p_box)
+                print(iou)
+                if iou > 0.05:
+                    self.people_detector.draw_resut(frame, people_classIds, people_scores, [p_box]) #TODO remove, it's for debug
+                    self.head_detector.draw_resut(frame, head_classIds, head_scores, [h_box]) 
+                    #histogram = histogram_finder.getHistogram(bouniding_box)
+                    #distance = distance_finder.findDistance(bouniding_box) -->creare oggetto per estrarre teste
+                    #histogram finder / salva dati riconoscimento
+                    #distance finderer
+                    
+    def calculate_iou(box_1, box_2):
+        poly_1 = Polygon([[box_1[0] , box_1[1]],
+                          [box_1[0]+box_1[2] , box_1[1]],
+                          [box_1[0]+box_1[2] , box_1[1]+box_1[3]],
+                          [box_1[0] , box_1[1]+box_1[3]]])
+        
+        poly_2 = Polygon([[box_2[0] , box_2[1]],
+                          [box_2[0]+box_2[2] , box_2[1]],
+                          [box_2[0]+box_2[2] , box_2[1]+box_2[3]],
+                          [box_2[0] , box_2[1]+box_2[3]]])
+        iou = poly_1.intersection(poly_2).area / poly_1.union(poly_2).area
+        return iou
