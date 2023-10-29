88f0285ced133f210da5b85d5a6a6db946eaa734
Federico Ramanzin <71649070+FedeRama@users.noreply.github.com>
2023-10-29 22:39:30 +0100

    set cpu usage and changed draw result to use pipeline output

--- src/detector/detector.py
+++ src/detector/detector.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from functions.frame_extractor import Frame_extractor
+from utils.functions.frame_extractor import Frame_extractor
 
 COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
@@ -16,12 +16,13 @@ class Detector():
         self.progressiveId = 0
 
     def detect(self, frame):
-        result = self.model.predict(frame, device=0, classes=[0])[0] #this is to pass detection of the first frame (we only give one)
+        result = self.model.predict(frame, device="cpu", classes=[0])[0] #this is to pass detection of the first frame (we only give one)
         return result.boxes.cpu().numpy().cls, result.boxes.cpu().numpy().conf, result.boxes.cpu().numpy().xyxy
 
-    def draw_resut(self, frame, classIds, scores, boxes, color):
-        for (classid, score, box) in zip( classIds, scores, boxes):
-                label = "%s : %f" % (self.classes[classid], score)
+    def draw_resut(self, frame, labels, boxes, color):
+        for (labe, box) in zip( labels, boxes):
+                #label = "%s : %f" % (self.classes[classid], score)
+                label = labe
                 cv2.rectangle(frame, np.asarray(box[:2], dtype = 'int'), np.asarray(box[2:4], dtype = 'int'), color, 2)
                 cv2.putText(frame, label, (int(box[0]), int(box[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         cv2.imshow("Detection result", frame)
